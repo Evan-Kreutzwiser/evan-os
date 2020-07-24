fdbe015eba11191000828c8d3b0a038b6b46581b
Evan Kreutzwiser <53940800+Evan-Kreutzwiser@users.noreply.github.com>
2020-07-24 13:37:57 -0400

    Initial Commit
    Preparing repo for existing code

---
+++ include/kernel.h
@@ -0,0 +1,13 @@
+/*
+ * evan-os/include/kernel.h
+ *
+ * Definitions the kernel's entry point and miscellaneous functions 
+ */
+
+#ifndef KERNEL_H
+#define KERNEL_H
+
+// Entry point
+void start(void);
+
+#endif // KERNEL_H
