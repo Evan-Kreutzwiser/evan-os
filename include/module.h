70bd2d8abf76ae340f96add983ca0d80049b7f0b
Evan-Kreutzwiser <evanpansage@gmail.com>
2020-07-26 14:26:01 -0400

    starting first driver

--- include/module.h
+++ include/module.h
@@ -18,27 +18,41 @@
 #define DEV_PIC					0x5
 	#define DEV_PIC_PIC			0x1
 	#define DEV_PIC_APIC		0x2
-#define DEV_PS2					0x6
+#define DEV_PS2					0x6 // PS2 devices
 	#define DEV_PS2_KEYBOARD	0x1
 	#define DEV_PS2_MOUSE		0x2
+#define DEV_IDE					0x7 // IDE drives
+	#define DEV_IDE_HDD			0x1 // IDE hard drive
+	#define DEV_IDE_CD			0x2 // IDE disk drive
 
 // Device connection type
 #define DEV_CONNECTION_DEV			0x0
 #define DEV_CONNECTION_MOTHERBOARD	0x1 // Generic board devices
 #define DEV_CONNECTION_PCI			0x2 // PCI devices
+#define DEV_CONNECTION_USB			0x3 // USB devices
 
 
 typedef struct driver_info_t {
-	char* name;
-	uint16_t type;
-	uint16_t connection_type;
-	void* info;
+	char* name; // The divice's human readable name
+	uint16_t type; // What type of device it is
+	uint16_t subtype;
+	uint16_t connection_type; // How the device is connected
+	void * init; // Pointer to the device initialization function
+	union {
+		struct {
+			uint16_t vendor;
+			uint16_t device;
+
+		} pci; 
+		struct {
+			void * end_of_int; // Pass a uint8_t holding the interrupt's index to all 3
+			void * mask_int;
+			void * unmsak_int; 
+		} pic;
+		
+
+	} connection_data;
 } driver_info_t;
-
-typedef struct drive_info_pci_t {
-
-	
-} drive_info_pci_t;
 
 uint8_t module_call_function(char* function_name);
 
