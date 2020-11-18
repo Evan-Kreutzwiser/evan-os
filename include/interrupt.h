321eb39221a7bc47a140328e0567d20c0321a76c
Evan-Kreutzwiser <evanpansage@gmail.com>
2020-11-17 19:22:29 -0500

    Moved PIC code from unused drive file to kernel

--- include/interrupt.h
+++ include/interrupt.h
@@ -30,13 +30,27 @@ struct interrupt_frame {
     uint64_t ss;
 };
 
+typedef void (*interrupt_handler_t) (void);
+
+
+void interrupt_init(void);
+
+void interrupt_handler(uint64_t interrupt_num);
+
 void interrupt_set_gate(uint8_t index, uint64_t address, uint8_t type_attributes);
+
+void interrupt_register(uint8_t index, interrupt_handler_t handler);
+void interrupt_unregister(uint8_t index);
 
 void interrupt_mask(uint8_t interrupt);
 void interrupt_unmask(uint8_t interrupt);
 
 void interrupt_load_table(void);
 
-void interrupt_set_mode(bool use_apic); // Set whether the OS will use the newer APIC or the old PIC for interrupts
+// Set whether the OS will use the newer APIC or the old PIC for interrupts
+void interrupt_set_mode(bool use_apic); 
+
+// Send an eoi to the PIC chips
+void interrupt_end_pic(uint8_t index);
 
 #endif // INTERRUPT_H
